a449f77f803ef3e864b7dc4d26831da9e9e6e386
Xuann642003 <xfuiaznzz@gmail.com>
2022-04-04 20:31:57 +0700

    A

---
+++ baituan1/Untitled127.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+main (){
+	int cd, cr; 
+	cout<<"Nhap chieu dai = ";cin>>cd;
+	cout<<"Nhap chieu rong = ";cin>>cr; 
+	for( int i = 1 ; i <= cr ; i++){
+		for (int j = 1 ; j <= cd ;j++){
+		cout<<"*"; 
+	}
+	cout<<endl; 
+} 
+}
